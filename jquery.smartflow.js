f16b95d0e52964a68b80cdc6820bfca82aa23c07
claudio <claudio@smop.org>
2013-06-28 12:56:48 +0200

    Refactoring the main routine (place()). Also fix #2.

--- jquery.smartflow.js
+++ jquery.smartflow.js
@@ -55,53 +55,7 @@ var
 		$w.data('scrollLeft', $w.scrollLeft());
 	},
 
-	isintoview = function(r) {
-		if(!r)
-			r = info.route;
-		switch(r) {
-			case UP:
-				t1 = info.wy + info.wh; // wbot
-				return (info.ey >= info.wy && info.ey <= t1);
-
-			case DOWN:
-				t1 = info.ey + info.eh; // ebot
-				t2 = info.wy + info.wh; // wbot
-				return (t1 >= info.wy && t1 <= t2);
-
-			case LEFT:
-				t1 = info.ex + info.ew; // eright
-				t2 = info.wx + info.ww; // wright
-				return (info.ex >= info.wx && t1 <= t2);
-
-			case RIGHT:
-				t1 = info.ex + info.ew; // eright
-				t2 = info.wx + info.ww; // wright
-				return (info.ex >= info.wx && t1 <= t2);
-		}
-	},
-
-	isintohook = function(e) {
-		hooks = $(e).data('hooks');
-
-		switch(info.route) {
-			case UP: return (info.ey < hooks.top);
-			case DOWN:
-				t1 = info.ey + info.eh; // ebot
-				return (hooks.bot < t1);
-		}
-
-		return false;
-	},
-
-	isvisible = function() {
-		t1 = info.ey + info.eh; // ebot
-		t2 = info.wy + info.wh; // wbot
-
-		return (t1 >= info.wy && info.ey <= t2);
-	},
-
 	flow = function(e) {
-		console.log('flow');
 		switch(info.route) {
 			case UP: style.top += diff; break;
 			case DOWN: style.top -= diff; break;
@@ -110,9 +64,10 @@ var
 		$(e).css({top:style.top+'px'});
 	},
 
-	fixit = function(e) {
-		console.log('fixit');
-		switch(info.route) {
+	fixit = function(e, r) {
+		if(!r)
+			r = info.route;
+		switch(r) {
 			case UP: style.top = 0; break;
 			case DOWN: style.top = info.wh - info.eh; break;
 		}
@@ -121,7 +76,6 @@ var
 	},
 
 	hook = function(e) {
-		console.log('hook');
 		hooks = $(e).data('hooks');
 
 		switch(info.route) {
@@ -147,20 +101,100 @@ var
 		elems.push(e);
 	},
 
-	place = function(e) {
-		info = getinfo(e);
+	getinfo = function(e) {
+		t1 = $w.scrollTop();
+		t2 = $w.data('scrollTop');
+		diff = t1 > t2 ? t1 - t2 : t2 - t1;
 
-		if(isintoview() && !isintohook(e)) {
-			if(info.eh < info.wh)
-				info.route = UP; /* XXX option */
-			fixit(e);
+		if(t1 != t2) {
+			route = t1 > t2 ? DOWN : UP;
 		}
-		else
-			flow(e);
+		else {
+			t1 = $w.scrollLeft();
+			t2 = $w.data('scrollLeft');
+			route = t1 > t2 ? RIGHT : LEFT;
+		}
+
+		return {
+			wx: $w.scrollLeft(),
+			wy: $w.scrollTop(),
+			ww: $w.width(),
+			wh: $w.height(),
+			ex: $(e).offset().left,
+			ey: $(e).offset().top,
+			ew: $(e).outerWidth(true),
+			eh: $(e).outerHeight(true),
+			route: route,
+			diff: diff
+		};
+	},
+
+	intoview = function(r) {
+		switch(r) {
+			case UP:
+				t1 = info.wy + info.wh; // wbot
+				return (info.ey >= info.wy && info.ey <= t1);
+
+			case DOWN:
+				t1 = info.ey + info.eh; // ebot
+				t2 = info.wy + info.wh; // wbot
+				return (t1 >= info.wy && t1 <= t2);
+		}
+	},
+
+	intohooks = function(e, r) {
+		hooks = $(e).data('hooks');
+
+		switch(r) {
+			case UP: return (info.ey >= hooks.top);
+			case DOWN:
+				t1 = info.ey + info.eh; // ebot
+				return (t1 <= hooks.bot);
+		}
+	},
+
+	invroute = function(r) {
+		switch(r) {
+			case UP: return DOWN;
+			case DOWN: return UP;
+			case LEFT: return RIGHT;
+			case RIGHT: return LEFT;
+		}
+	},
+
+	place = function(e) {
+
 
 		info = getinfo(e);
-		if(!isvisible() || isintohook(e))
-			hook(e);
+		flow(e);
+
+		info = getinfo(e);
+		if(!intoview(invroute(info.route))) {
+			fixit(e, invroute(info.route));
+
+			info = getinfo(e);
+			if(!intohooks(e, info.route)) {
+				hook(e, info.route);
+			}
+		}
+		else if(info.eh < info.wh) {
+			info = getinfo(e);
+			if(!intohooks(e, UP)) {
+				hook(e, UP);
+			}
+			else {
+				fixit(e, UP);
+				info = getinfo(e);
+
+				if(!intohooks(e, invroute(info.route))) {
+					info.route = invroute(info.route);
+					hook(e, info.route);
+				}
+				else if(!intohooks(e, info.route)) {
+					hook(e, info.route);
+				}
+			}
+		}
 
 		update();
 	};
