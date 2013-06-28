bitsmanent/jquery.smartflow

commit 344ef911ddadc18717e20eb76cbbd4b8a5f357d0

Fix #1.

jquery.smartflow.js
@@ -163,38 +163,44 @@ var
 	},
 
 	place = function(e) {
-
-
 		info = getinfo(e);
 		flow(e);
 
 		info = getinfo(e);
-		if(!intoview(invroute(info.route))) {
+		if(info.eh < info.wh) {
+			console.log('1');
+			info = getinfo(e);
+			if(!intohooks(e, UP)) {
+				console.log('2');
+				hook(e, UP);
+			}
+			else {
+				console.log('3');
+				fixit(e, UP);
+				info = getinfo(e);
+
+				if(!intohooks(e, invroute(info.route))) {
+					console.log('4');
+					info.route = invroute(info.route);
+					hook(e, info.route);
+				}
+				else if(!intohooks(e, info.route)) {
+					console.log('5');
+					hook(e, info.route);
+				}
+			}
+		}
+		else if(!intoview(invroute(info.route))) {
+			console.log('a');
 			fixit(e, invroute(info.route));
 
 			info = getinfo(e);
 			if(!intohooks(e, info.route)) {
 				hook(e, info.route);
+				console.log('b');
 			}
 		}
-		else if(info.eh < info.wh) {
-			info = getinfo(e);
-			if(!intohooks(e, UP)) {
-				hook(e, UP);
-			}
-			else {
-				fixit(e, UP);
-				info = getinfo(e);
 
-				if(!intohooks(e, invroute(info.route))) {
-					info.route = invroute(info.route);
-					hook(e, info.route);
-				}
-				else if(!intohooks(e, info.route)) {
-					hook(e, info.route);
-				}
-			}
-		}
 
 		update();
 	};

commit 95ea6fe7a4b6d61621ff28b660daad17ffe5eb15

Fix a bug introduced with 344ef911ddadc18717e20eb76cbbd4b8a5f357d0.

jquery.smartflow.js
@@ -185,15 +185,15 @@ var
 				}
 			}
 		}
-		else if(!intoview(invroute(info.route))) {
-			fixit(e, invroute(info.route));
+		else {
+			if(intoview(info.route) || !intoview(invroute(info.route)))
+				fixit(e, info.route);
 
 			info = getinfo(e);
 			if(!intohooks(e, info.route)) {
 				hook(e, info.route);
 			}
 		}
-
 
 		update();
 	};